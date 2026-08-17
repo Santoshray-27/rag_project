cb2491afb203d90b84e592a8b4640546fd22b871
Santosh Ray <iamsantoshray27@gmail.com>
2026-08-17 22:43:54 +0530

    Day2

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 const app = express();
+const PORT = process.env.PORT || 5000
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.json({ message: 'Documind Backend is running!!!✅✅✅' })
 })
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000')
+app.listen(PORT, () => {
+    console.log(`Server running on port  ${PORT}`)
 })
