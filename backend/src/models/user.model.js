fdc17e0de479268b6803aee7ed95a4cf8434ba84
Santosh Ray <iamsantoshray27@gmail.com>
2026-08-27 23:43:10 +0530

    feat(auth): create user model with bcrypt password hashing

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true, // No two users can have the same email
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+  },
+  {
+    timestamps: true, // Automatically creates createdAt and updatedAt fields
+  }
+);
+
+// Hash the password before saving to the database
+userSchema.pre("save", async function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // Generate a 'salt' with 10 rounds (standard complexity)
+  const salt = await bcrypt.genSalt(10);
+  // Hash the password with the salt
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Create a method to compare entered password with the hashed password in DB
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
